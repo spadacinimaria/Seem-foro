5e7a05443786cf6ceae2f4458774bc7a500ac21b
spadacinimaria <165792927+spadacinimaria@users.noreply.github.com>
2024-09-13 09:51:24 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const vermelho = document.getElementById('vermelho')
+const amarelo = document.getElementById('amarelo')
+const verde = document.getElementById('verde')
+
+let estadoAtual = 'amarelo'
+
+function alternarluzes(){
+    if(estadoAtual === 'amarelo'){
+        vermelho.style.backgroundColor = '#555555';
+        amarelo.style.backgroundColor = '#ffff00';
+        verde.style.backgroundColor = '#555555';
+        estadoAtual = 'vermelho'; 
+    }else if(estadoAtual === 'vermelho'){
+        vermelho.style.backgroundColor = '#ff0000';
+        amarelo.style.backgroundColor = '#555555';
+        verde.style.backgroundColor = '#555555';
+        estadoAtual = 'verde';
+    }else if(estadoAtual === 'verde'){
+        vermelho.style.backgroundColor = '#555555';
+        amarelo.style.backgroundColor = '#555555';
+        verde.style.backgroundColor = '#00ff00';
+        estadoAtual = 'amarelo';
+    }
+}
+setInterval(alternarluzes, 2000);
+});
